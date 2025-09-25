aa19d6f5c264971a14b3ec2d7b27a488265374ae
Amine Benmoussa <121407324+Amine-Nova@users.noreply.github.com>
2025-09-25 21:26:35 +0100

    try to fusin the two sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,41 @@
 import "./App.css";
 import MainSection from "./components/section1/mainSection";
 import ClickSpark from "./components/spark/spark";
-import LiquidEther from "./components/Squares/squares";
+import LiquidEther from "./components/Backgrounds/rails";
 import SecondSection from "./components/SecondSection/second";
+
 
 // With custom prop values
 function App() {
   return (
-    <ClickSpark className="fixed w-full h-full"
+    <ClickSpark
+      className="fixed w-full h-full"
       sparkColor="#fff"
       sparkSize={30}
       sparkRadius={60}
       sparkCount={5}
       duration={700}
     >
-      <section className="w-full flex flex-col items-center relative">
-      <div style={{width: '100%', height: '100%',  position: "absolute" }}>
-        <LiquidEther
-          colors={["#2e2946", "#16141e", "#3c3464"]}
-          mouseForce={20}
-          cursorSize={100}
-          isViscous={false}
-          viscous={30}
-          iterationsViscous={32}
-          iterationsPoisson={32}
-          resolution={0.5}
-          isBounce={false}
-          autoDemo={true}
-          autoSpeed={0.3}
-          autoIntensity={2.2}
-          takeoverDuration={0.25}
-          autoResumeDelay={3000}
-          autoRampDuration={0.6}
-        />
-      </div>
+      <section className="w-full flex flex-col items-center relative b">
+        <div style={{ width: "100%", height: "100%", position: "absolute" }}>
+          <LiquidEther
+            colors={["#2e2946", "#16141e", "#3c3464"]}
+            mouseForce={20}
+            cursorSize={100}
+            isViscous={false}
+            viscous={30}
+            iterationsViscous={32}
+            iterationsPoisson={32}
+            resolution={0.5}
+            isBounce={false}
+            autoDemo={true}
+            autoSpeed={0.3}
+            autoIntensity={2.2}
+            takeoverDuration={0.25}
+            autoResumeDelay={3000}
+            autoRampDuration={0.6}
+          />
+        </div>
         <div className="relative w-full flex flex-col items-center gap-20">
           <MainSection />
         </div>
--- src/components/SecondSection/second.jsx
+++ src/components/SecondSection/second.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Text from '../HeyText/text'
+import LightRays from "../Backgrounds/lightup";
 
 const second = () => {
   return (
     <div className='flex w-full justify-center relative max-w-full'>
-      <Text />
+        <Text />
+      <div style={{ width: "100%", height: "600px", position: "absolute" }}>
+          <LightRays
+            raysOrigin="top-center"
+            raysColor="#00ffff"
+            raysSpeed={1.5}
+            lightSpread={0.8}
+            rayLength={1.2}
+            followMouse={true}
+            mouseInfluence={0.1}
+            noiseAmount={0.1}
+            distortion={0.05}
+            className="custom-rays"
+          />
+        </div>
     </div>
   )
 }
