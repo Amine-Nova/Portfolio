c2bada64f81855defc73fdbe564a202709ac1f18
Amine-Nova <bmamine6@gmail.com>
2025-09-26 23:23:51 +0100

    set animations

--- src/components/Content/content.jsx
+++ src/components/Content/content.jsx
@@ -1,19 +1,33 @@
 import React from "react";
+import SplitText from "../Animations/text";
 
 const content = () => {
+  const handleAnimationComplete = () => {
+    console.log("All letters have animated!");
+  };
   return (
     <div className="flex gap-5">
       <div className="txt text-white font-bold cursor-default leading-tight min-h-[calc(80vh-146px)] pb-20 uppercase flex flex-col gap-4 justify-center items-center select-none">
-        <p className="text-4xl tracking-wide sm:text-5xl md:text-8xl">
-          Hey There,
-        </p>
+
+          <SplitText
+            text="Hey There,"
+            className="text-4xl tracking-wide sm:text-5xl md:text-8xl"
+            delay={100}
+            duration={0.6}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+            onLetterAnimationComplete={handleAnimationComplete}
+          />
         <p className="text-4xl tracking-wide sm:text-5xl md:text-8xl">
           Amine Here
         </p>
       </div>
-      <div className="flex justify-center items-center pb-12 rounded">
-      </div>
-      
+      <div className="flex justify-center items-center pb-12 rounded"></div>
     </div>
   );
 };
