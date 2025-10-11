46900f56b01d4457706da8ad12676604c12ed6ae
Amine Benmoussa <121407324+Amine-Nova@users.noreply.github.com>
2025-10-11 18:32:44 +0100

    conatainers set

--- src/components/Containers/container.jsx
+++ src/components/Containers/container.jsx
@@ -2,18 +2,41 @@ import React from 'react'
 
 const container = () => {
   return (
-    <div className='grid grid-cols-3 gap-20 px-4 py-20 relative'>
-      <div className='w-[300px] h-[80px] bg-green-900 border-blue-800'>
-        container
-      </div>
-      <div className='w-[300px] h-[80px] bg-green-900 border-blue-800'>
-        container
-      </div>
-      <div className='w-[300px] h-[80px] bg-green-900 border-blue-800'>
-        container
-      </div>
+    <div className='grid grid-cols-3 gap-20 px-4 py-20'>
+      <button className='group relative transition-all ease-in-out fade-in'>
+        <div className='container'>
+            <div className='flex gap-4 py-6 px-8 rounded-3xl h-32 items-center bg-black ring-1 ring-blue-400 relative top-0 left-0 group-hover:-left-2 transition-all ease-in-out'>
+              <img className='w-16 h-16 rounded-sm' src="" alt="" />
+              <div className='flex flex-col gap-1 test-start relative'>
+                <p className='text-blue-200 font-semibold max-w-[20ch] overflow-hodden'>Pong Arena</p>
+              </div>
+            </div>
+        </div>
+      </button>
+      <button className='group relative transition-all ease-in-out fade-in'>
+        <div className='container'>
+            <div className='flex gap-4 py-6 px-8 rounded-3xl h-32 items-center bg-black ring-1 ring-blue-400 relative top-0 left-0 group-hover:-left-2 transition-all ease-in-out'>
+              <img className='w-16 h-16 rounded-sm' src="" alt="" />
+              <div className='flex flex-col gap-1 test-start relative'>
+                <p className='text-blue-200 font-semibold max-w-[20ch] overflow-hodden'>Pong Arena</p>
+              </div>
+            </div>
+        </div>
+      </button>
+      <button className='group relative transition-all ease-in-out fade-in'>
+        <div className='container'>
+            <div className='flex gap-4 py-6 px-8 rounded-3xl h-32 items-center bg-black ring-1 ring-blue-400 relative top-0 left-0 group-hover:-left-2 transition-all ease-in-out'>
+              <img className='w-16 h-16 rounded-sm' src="" alt="" />
+              <div className='flex flex-col gap-1 test-start relative'>
+                <p className='text-blue-200 font-semibold max-w-[20ch] overflow-hodden'>Pong Arena</p>
+              </div>
+            </div>
+        </div>
+      </button>
     </div>
   )
 }
 
 export default container
+
+
--- src/components/Content/content.jsx
+++ src/components/Content/content.jsx
@@ -7,24 +7,10 @@ const Content = () => {
   return (
     <div className="flex flex-col w-full items-center justify-center txt text-white lg:gap-1 min-h-[35em] sm:min-h-[28em] md:min-h-[50em] lg:min-h-[58em] 2xl:min-h-[62em]">
       <div className="font-bold cursor-default leading-tight flex xl:gap-4 gap-3 justify-center items-center select-none">
-      <SplitText
-          text={`👨🏻‍💻`}
-          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl pb-2"
-          delay={100}
-          duration={0.6}
-          ease="power3.out"
-          splitType="chars"
-          from={{ opacity: 0, y: 40 }}
-          to={{ opacity: 1, y: 0 }}
-          threshold={0.1}
-          rootMargin="-100px"
-          textAlign="center"
-          onLetterAnimationComplete={handleAnimationComplete}
-        />
-        <p className=""></p>
+          <p className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl pb-2">👨🏻‍💻</p>
         <SplitText
           text={`Hey, it’s Amine`}
-          className="tracking-wide text-[43px] sm:text-6xl md:text-7xl lg:text-8xl pb-2"
+          className=" wave-text tracking-wide text-[43px] sm:text-6xl md:text-7xl lg:text-8xl pb-2"
           delay={100}
           duration={0.6}
           ease="power3.out"
