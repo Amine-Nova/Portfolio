c06b533ff562998f09bd68a8c668f1526b6356ee
Amine Benmoussa <121407324+Amine-Nova@users.noreply.github.com>
2025-10-11 15:15:26 +0100

    set projects section

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className='relative'>
-    <p className="segoe text-4xl sm:text-6xl md:text-7xl lg:text-9xl text-blue-200 capitalize overflow-hidden">projects</p>
+    <p className="segoe text-4xl sm:text-6xl md:text-7xl lg:text-9xl text-blue-200 capitalize">projects</p>
   </div>
   )
 }
